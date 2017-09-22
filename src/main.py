42e33b0399f54ed254dff6b86f842497af52c5d5
George Lacey <git@george.ooo>
2017-09-22 16:45:02 +0100

    Create population object

--- src/main.py
+++ src/main.py
@@ -1,13 +1,10 @@
 import argparse
-from individual import Individual
+from population import Population
 
 args = argparse.ArgumentParser()
 args.add_argument("pop", help="Population size", type=int)
 args = args.parse_args()
 
-population = []
+pop = Population(10)
 
-for current in range(0, args.pop):
-    population.append(Individual())
-
-print(len(population))
+print("%d" % pop.fitness_function())
