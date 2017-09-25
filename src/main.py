af16b969b4c9e4ef48994da95f8cdb92ca94e107
George Lacey <git@george.ooo>
2017-09-25 19:03:03 +0100

    Loop through generations

--- src/main.py
+++ src/main.py
@@ -6,3 +6,8 @@ args.add_argument("pop", help="Population size", type=int)
 args = args.parse_args()
 
 pop = Population(args.pop)
+
+while True:
+    print(pop)
+    input("Advance...")
+    pop.advance_generation()
