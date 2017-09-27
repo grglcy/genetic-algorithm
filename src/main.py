ecd8e35888e4ecb1727f382e0301afdc7b3babbc
George Lacey <git@george.ooo>
2017-09-27 13:50:13 +0100

    Iterate 1000 times and print

--- src/main.py
+++ src/main.py
@@ -7,8 +7,7 @@ args = args.parse_args()
 
 pop = Population(args.pop)
 
-for i in range(0, 5000):
+for i in range(0, 1000):
     pop.advance_generation()
     best = pop.best_fitness()
-    print("Best fit:\tx:%e,\ty:%e\tf:%e" % (best.x, best.y, best.fitness()))
-    print("Average fit:\t%e\n" % pop.avg_fitness())
+    print(pop)
